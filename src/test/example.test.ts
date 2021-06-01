e9d4a29515e384ac27fcea367b6b4080394ae47b
Hudson Steffani Soares Rosa <hudsonssrosa@gmail.com>
2021-06-01 12:18:01 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,38 +1,47 @@
 import {isLeapYear} from "../main/example";
+import each from 'jest-each';
 
-describe('example test', () => {
-    it("should return false if not divisible by 4", () => {
-        let leapYear = isLeapYear(2001);
-        expect(leapYear).toBe(false);
-    })
 
-    it("should return true if is divisible by 4", () => {
-        let leapYear = isLeapYear(2016);
-        expect(leapYear).toBe(true);
-    })
+describe('.isLeapYear(year)', () => {
+    each([
+        [2001, false],
+        [2016, true],
+        [1600, true],
+        [1800, false],
+        [0, true],
+        [-400, true]
+    ]).test('should return false if not divisible by 4', (year: number, expected: boolean) => {
+        expect(isLeapYear(year)).toBe(expected);
+    });
 
-    it("should return true if its divisible by 400", () => {
-        let leapYear = isLeapYear(1600);
-        expect(leapYear).toBe(true);
-    })
+    // it("should return false if not divisible by 4", () => {
+    //     let leapYear = isLeapYear(2001);
+    //     expect(leapYear).toBe(false);
+    // })
 
-    it("should return false if its divisible by 100", () => {
-        let leapYear = isLeapYear(1800);
-        expect(leapYear).toBe(false);
-    })
+    // it("should return true if is divisible by 4", () => {
+    //     let leapYear = isLeapYear(2016);
+    //     expect(leapYear).toBe(true);
+    // })
 
-    it("should return true if its divisible by 0", () => {
-        let leapYear = isLeapYear(0);
-        expect(leapYear).toBe(true);
-    })
+    // it("should return true if its divisible by 400", () => {
+    //     let leapYear = isLeapYear(1600);
+    //     expect(leapYear).toBe(true);
+    // })
 
-    it("should return true if its divisible by -400", () => {
-        let leapYear = isLeapYear(-400);
-        expect(leapYear).toBe(true);
-    })
+    // it("should return false if its divisible by 100", () => {
+    //     let leapYear = isLeapYear(1800);
+    //     expect(leapYear).toBe(false);
+    // })
 
-    it("should return true if its divisible by 400.1", () => {
-        let leapYear = isLeapYear(400.1);
-        expect(leapYear).toBe(true);
-    })
+    // it("should return true if its divisible by 0", () => {
+    //     let leapYear = isLeapYear(0);
+    //     expect(leapYear).toBe(true);
+    // })
+
+    // it("should return true if its divisible by -400", () => {
+    //     let leapYear = isLeapYear(-400);
+    //     expect(leapYear).toBe(true);
+    // })
+
 })
