62560e135bad512c7d7311d671551940af434e13
Hudson Steffani Soares Rosa <hudsonssrosa@gmail.com>
2021-06-01 11:01:39 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,8 +1,13 @@
 import {isLeapYear} from "../main/example";
 
 describe('example test', () => {
-    it("should return false if not divisble by 4", () => {
+    it("should return false if not divisible by 4", () => {
         let leapYear = isLeapYear(2001);
         expect(leapYear).toBe(false);
+    })
+
+    it("should return false if is divisible by 4", () => {
+        let leapYear = isLeapYear(2016);
+        expect(leapYear).toBe(true);
     })
 })
